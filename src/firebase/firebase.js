bc9a04c94935aa77c03f5809d7e221cce03df290
Sam Reichle <samjhreichle@gmail.com>
2020-08-01 18:22:33 -0500

    Made some necessary changes to webpack for firebase to run correctly

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,17 @@
 import * as firebase from 'firebase';
 
-const config = {
+const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: "1:574668724764:web:8bc9abf120853282769dd9",
+  measurementId: "G-PFBNL2B0Y0"
 };
 
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
